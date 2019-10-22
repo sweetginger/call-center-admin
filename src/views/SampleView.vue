8836056013cada5d39065612219de8ad8d72014e
sweetginger <hyr2902@gmail.com>
2019-10-23 02:46:34 +0900

    레이아웃 잡는중

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -20,15 +20,18 @@
                   </span>
                 </div>
               </li>
-              <li>
-                <div class="callStatusArea">
+              <li class="callStatusArea">
+                <div>
                   <div class="timer">
-                    <span>상담 대기중</span>
-                    02 : 26 : 54
+                    <span class="statusMsg">상담 대기중</span>
+                    <span class="primary">02 : 26 : 54</span>
                   </div>
                   <div class="btns">
-                    <div class="callOn">상담대기</div>
-                    <ul>
+                    <!-- <div class="callOn">상담대기</div> -->
+                    <ul class="callOn">
+                      <li>상담대기</li>
+                    </ul>
+                    <ul class="workStatus">
                       <li>자리비움</li>
                       <li>작업</li>
                       <li>식사</li>
@@ -36,18 +39,27 @@
                   </div>
                 </div>
               </li>
-              <li>
-                <div class="callStats">
+              <li class="callStats">
+                <div>
                   <ul>
                     <li>
-                      인입
-                      <span>2</span>
+                      <span>인입</span>
+                      <span class="count">203</span>
                     </li>
-                    <li>발신</li>
+                    <li>
+                      <span>발신</span>
+                      <span class="count">9</span>
+                    </li>
                   </ul>
                   <ul>
-                    <li>콜백</li>
-                    <li>대기</li>
+                    <li>
+                      <span>콜백</span>
+                      <span class="count">2</span>
+                    </li>
+                    <li>
+                      <span>대기</span>
+                      <span class="count">11</span>
+                    </li>
                   </ul>
                 </div>
               </li>
@@ -61,11 +73,21 @@
             <div class="title">
               <div class="profile">
                 <img src="@/assets/img/icons8-circled-user-64.png" />
-                <span>홍길순 상담원</span>
-                <ul class="info">
-                  <li>그룹 - 국내여행</li>
-                  <li>내선 - 6506</li>
-                </ul>
+                <div class="btns">
+                  <i class="fas fa-cog"></i>
+                  <i class="fas fa-sign-out-alt"></i>
+                </div>
+                <div>홍길순 상담원</div>
+                <div>
+                  <ul class="info">
+                    <li class="title">그룹</li>
+                    <li>국내여행</li>
+                  </ul>
+                  <ul class="info">
+                    <li class="title">내선</li>
+                    <li>6506</li>
+                  </ul>
+                </div>
               </div>
             </div>
             <ul class="list">
@@ -146,7 +168,93 @@
         </section>
         <section id="section2">
           <div class="section">
-            <div class="title">배경이미지</div>
+            <div class="title">나의 상담목록</div>
+            <div class="tabArea">
+              <ul>
+                <li>최근 수발신</li>
+                <li>콜백</li>
+              </ul>
+            </div>
+            <div class="tab tab1">
+              수발신
+              <select>
+                <option>전체 (5)</option>
+              </select>
+              <input type="text" placeholder="고객명 전화번호 검색" />
+              <ul>
+                <li>
+                  <ul class="callItem">
+                    <li>
+                      <i class="fas fa-star"></i>
+                    </li>
+                    <li>수신</li>
+                    <li>-</li>
+                    <li>02-3434-2323</li>
+                  </ul>
+                </li>
+                <li>
+                  <ul class="callItem">
+                    <li>
+                      <i class="fas fa-star"></i>
+                    </li>
+                    <li>수신</li>
+                    <li>-</li>
+                    <li>02-3434-2323</li>
+                  </ul>
+                </li>
+                <li>
+                  <ul class="callItem">
+                    <li>
+                      <i class="fas fa-star"></i>
+                    </li>
+                    <li>수신</li>
+                    <li>-</li>
+                    <li>02-3434-2323</li>
+                  </ul>
+                </li>
+                <li>
+                  <ul class="callItem">
+                    <li>
+                      <i class="fas fa-star"></i>
+                    </li>
+                    <li>수신</li>
+                    <li>-</li>
+                    <li>02-3434-2323</li>
+                  </ul>
+                </li>
+                <li>
+                  <ul class="callItem">
+                    <li>
+                      <i class="fas fa-star"></i>
+                    </li>
+                    <li>수신</li>
+                    <li>-</li>
+                    <li>02-3434-2323</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <!--
+            <div class="tab tab2">
+              콜백
+              <select>
+                <option>전체 (5)</option>
+              </select>
+              <input type="text" placeholder="고객명 전화번호 검색" />
+              <ul>
+                <li>
+                  <ul class="callItem">
+                    <li>
+                      <i class="fas fa-star"></i>
+                    </li>
+                    <li>수신</li>
+                    <li>-</li>
+                    <li>02-3434-2323</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            -->
           </div>
         </section>
       </section>
@@ -167,7 +275,7 @@ export default {};
 * {
   margin: 0;
   padding: 0;
-  font-size: 20px;
+  font-size: 16px;
 }
 
 ul,
@@ -178,26 +286,22 @@ li {
 /*탑버튼*/
 .btn-top {
   position: fixed;
-  bottom: 200px;
+  bottom: 80px;
   right: 80px;
   width: 50px;
   height: 50px;
-  background-color: pink;
+  background-color: #ddd;
+  border-radius: 100px;
+  line-height: 50px;
 }
-
+#wrap {
+  width: 1500px;
+  margin: auto;
+}
 /*header*/
 header {
-  background-color: lightblue;
   height: 100px;
-}
-
-.logo {
-  width: 200px;
-  margin-right: 0;
-}
-
-.logo img {
-  width: 135px;
+  border-bottom: 1px solid #ddd;
 }
 
 .gnb {
@@ -206,7 +310,9 @@ header {
   height: 100px;
   box-sizing: border-box;
 }
-
+.gnb > ul > li {
+  /* width: 400px; */
+}
 .gnb li {
   float: left;
   margin-right: 30px;
@@ -237,7 +343,7 @@ section#contents section {
     가장 기본 , flex-grow 증가너비 / flex-shrink 감소너비  / flex-basis 기본너비
     flex-basis 의 기본값은 auto 이지만 단축속성에서 사용할 경우 0 적용
     flex:1; = flex:1 1; = flex:1 1 0;
-    가변너비가 아니거나 값이 0일 경우 효과가 없음! 
+    가변너비가 아니거나 값이 0일 경우 효과가 없음!
     */
   height: 100%;
 }
@@ -250,7 +356,12 @@ section#contents nav {
 
 #nav {
   height: 100%;
-  background-color: rgb(150, 149, 149);
+  /* background-color: rgb(150, 149, 149); */
+  border-right: 1px solid #ddd;
+}
+
+#nav .list {
+  margin-top: 100px;
 }
 
 .list {
@@ -262,7 +373,14 @@ section#contents nav {
 .list-item {
   height: 50px;
   line-height: 50px;
-  background-color: salmon;
+  background-color: #ddd;
+  text-align: left;
+  padding-left: 15px;
+}
+.list-item i {
+  width: 30px;
+  text-align: center;
+  margin-right: 10px;
 }
 /*
 .list-item:nth-child(2n) {
@@ -295,7 +413,6 @@ div.section {
 }
 
 #section1 {
-  background-color: tan;
 }
 
 #section1 table {
@@ -322,17 +439,13 @@ div.section {
 }
 
 #section2 {
-  background-color: khaki;
+  /* background-color: khaki; */
   flex: none;
-}
-
-#section2 {
-  background: khaki url("/assets/img/dog.jpg") no-repeat center;
+  /* background: khaki url("/assets/img/dog.jpg") no-repeat center; */
   background-size: cover;
   /* width: 100%; */
   width: 200px;
-}
-#section2 .bg {
+  border-left: 1px solid #ddd;
 }
 
 /* footer */
@@ -347,5 +460,127 @@ footer {
   height: 150px;
   text-align: center;
   line-height: 150px;
+}
+.gnb .callBtnArea {
+  width: 380px;
+  padding: 20px 0;
+  text-align: left;
+}
+.gnb .callBtnArea input {
+  height: 37.78px;
+  -webkit-appearance: none;
+  border-style: none;
+  border: 1px solid #ccc;
+  padding: 0 8px;
+}
+.gnb .callBtnArea .callBtns span {
+  padding: 10px;
+  background: #ddd;
+  border-radius: 5px;
+  margin-left: 2px;
+}
+.gnb .callBtnArea .callBtns span.call {
+  padding: 10px 25px;
+  background: #23b2ec;
+}
+.gnb .callStatusArea {
+  width: 400px;
+}
+.gnb .callStatusArea .timer {
+  float: left;
+}
+
+.callBtnArea .callBtns {
+  display: inline-block;
+}
+.callStatusArea .timer span {
+  display: block;
+}
+.callStatusArea .btns ul {
+  padding: 0 5px;
+  margin-right: 5px;
+}
+.callStatusArea .btns .callOn {
+  float: left;
+  line-height: 79px;
+  height: 80px;
+  background: #87ceeb;
+}
+.callStatusArea .btns .callOn li {
+  height: 100%;
+}
+.callStatusArea .btns ul {
+  float: left;
+  border-radius: 5px;
+}
+.callStatusArea .btns ul li {
+  float: none;
+  height: 24px;
+  margin: 2px;
+  /* background: #ddd; */
+}
+.callStatusArea .workStatus {
+  background: #ddd;
+  /* border-radius: 5px; */
+}
+.callStatusArea .workStatus li {
+  font-size: 12px;
+  line-height: 24px;
+  /* border-top: 1px solid #fff; */
+  border-bottom: 1px solid #efefef;
+}
+.callStatusArea .workStatus li:first-child {
+  border-top: none;
+}
+.callStatusArea .workStatus li:last-child {
+  border: none;
+}
+.logo {
+  width: 200px !important;
+  margin-right: 0 !important;
+  padding: 27px;
+}
+.logo img {
+  width: 135px;
+}
+.callStats ul li {
+  height: 30px;
+  width: 85px;
+  text-align: left;
+}
+.profile .btns i {
+  width: 50%;
+  padding: 15px 25px;
+}
+.callStatusArea .timer {
+  padding: 0 20px;
+}
+.callStatusArea .timer .statusMsg {
+  text-align: left;
+  font-size: 13px;
+  padding-top: 8px;
+}
+.callStatusArea .timer .primary {
+  padding-top: 10px;
+  font-size: 25px;
+  font-weight: 600;
+}
+.callStats {
+  padding: 14px 0;
+}
+.callStats li span {
+  padding: 3px 8px;
+}
+.callStats li span.count {
+  background: #87ceeb69;
+  border-radius: 20px;
+  font-size: 12px;
+}
+.profile .info li {
+  float: left;
+  /* padding: 0 15px; */
+}
+.profile .info .title {
+  padding: 0 15px;
 }
 </style>
